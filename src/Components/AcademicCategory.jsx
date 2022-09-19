e59c3cc70e2d0e061ff2ebf559c424185db8bdf0
Tshepi <tshepi.tech@gmail.com>
2022-09-19 15:49:47 +0200

    filter components created

---
+++ src/Components/AcademicCategory.jsx
@@ -0,0 +1,19 @@
+//Project Files
+import BursaryItem from "./BursaryItem.jsx";
+
+export default function AcademicCategory({value,bursaries}) {
+  
+
+      //Properties
+      const selected_category = bursaries.filter((item) => item.category === `${value}`);
+      const category = selected_category.map((item) => (
+        <BursaryItem key={item.id} item={item} />
+      ));
+    
+      return (
+        <div >
+          <div>{category}</div>
+        </div>
+      );
+    
+}
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,29 +2,32 @@
 import { useEffect, useState } from "react";
 //Project files
 import bursaries from "../Data/bursaries.json";
-import BursaryList from "./BursaryList.jsx"
+import BursaryList from "./BursaryList.jsx";
+import Filters from "./Filters.jsx";
 
 import SearchList from "./SearchList";
 
 export default function Dashboard() {
 
+   //Local State
+   const [searchField, setSearchField] = useState("");
+
    //Methods
    const handleChange = (event) => {
     setSearchField(event.target.value);
   };
+
+  //Properties
   const searchbox = <input
   className="search__box"
   type="search"
-  placeholder="Search try"
+  placeholder="Search company"
   onChange={handleChange}
 />
 
-  //Local State
-  const [searchField, setSearchField] = useState("");
-
-
   return (
     <div>
+      <Filters bursaries={bursaries}/>
       {searchbox}
        {searchbox === "" ? (<BursaryList/>):
   (<SearchList bursaries={bursaries} searchField={searchField} />)}
